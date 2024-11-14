8a32a50d9c80f384dd8ad217f0195aa9952d41c2
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-11-14 12:01:20 +0900

    練習問題②Q1の回答を記述

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,6 +1,19 @@
 <?php
 // Q1 tic-tac問題
+<?php
 
+echo '1から100までのカウントを開始します' ."\n" ."\n";
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 4 == 0 && $i % 5 == 0) {
+      echo 'tic-tac' ."\n";
+    } elseif ($i % 5 == 0) {
+      echo 'tac' ."\n";
+    } elseif ($i % 4 == 0) {
+      echo 'tic' ."\n";
+    } else {
+      echo "$i" ."\n";
+    }
+}
 
 // Q2 多次元連想配列
 
