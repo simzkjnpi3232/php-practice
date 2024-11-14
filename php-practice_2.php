c08c0a02fb73da6758fe0fb7b59e8ecb4ca55334
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-11-15 00:27:56 +0900

    練習問題②Q2-1の回答を記述

--- php-practice_2.php
+++ php-practice_2.php
@@ -16,6 +16,35 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 // Q2 多次元連想配列
+<?php
+
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
+
+echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。' ."\n";
+
+$ageList = [25, 30, 18];
+
+foreach ($ageList as $key => $age) {
+$personalInfos[$key]['age'] = $age;
+}
+
+var_dump($personalInfos);
 
 
 // Q3 オブジェクト-1
@@ -25,4 +54,8 @@ for ($i = 1; $i <= 100; $i++) {
 
 
 // Q5 定義済みクラス
+
+
 ?>
+
+
